f69990878d1cf3c5dbf84bb5e657f2745a2a481e
petrix <yuri.petrik@gmail.com>
2018-05-15 17:07:33 +0300

    Update customcountdown_module.js

--- public/js/customcountdown_module.js
+++ public/js/customcountdown_module.js
@@ -16,13 +16,15 @@ function customcountdown_module(timeSelector, titleSelector, alwaysActive, count
             (data.duration >0) ? minutes = Math.floor(data.duration / 60): minutes = Math.abs(Math.ceil(data.duration / 60));
             (data.duration >0) ? seconds = data.duration - (minutes * 60): seconds = Math.abs(data.duration + (minutes * 60));
             
+            
+            
             seconds = seconds.toFixed(0);
             hours = ((hours < 10 && hours >= 0) ? "0" : "") + hours;
             minutes = ((minutes < 10 && minutes >= 0) ? "0" : "") + minutes;
-            if (data.duration < 0) {hours = "-" + hours}
+            if (data.duration < 0) {minutes = "-" + minutes}
             seconds = ((seconds < 10 && seconds >= 0) ? "0" : "") + seconds;
 
-            $(timeSelector).text(hours + ':' + minutes + ':' + seconds);
+            $(timeSelector).text(minutes + ':' + seconds);
         }
     });
 }
