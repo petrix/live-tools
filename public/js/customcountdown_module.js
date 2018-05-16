990688fed2f36dad0277af8147b857c86bd3b4f7
petrix <yuri.petrik@gmail.com>
2018-05-16 16:04:58 +0300

    Update customcountdown_module.js

--- public/js/customcountdown_module.js
+++ public/js/customcountdown_module.js
@@ -33,7 +33,7 @@ function customcountdown_module(timeSelector, titleSelector, alwaysActive, count
             if (seconds <= 15){
                 setInterval(function() {$(timeSelector).css( "color" , "#ff0000" );}
                 setInterval(function() {$(timeSelector).css( "color" , "#000000" );}, 500);
-            //}
+            }
             
             //if (data.duration < 15) setInterval(function() {
            //            $(timeSelector).css( "color" , "#ff0000" ); 
