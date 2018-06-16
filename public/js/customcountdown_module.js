62a17ae01883856865e0571e578209c7a0c5c50a
petrix <yuri.petrik@gmail.com>
2018-06-16 06:40:12 +0300

    xxx

--- public/js/customcountdown_module.js
+++ public/js/customcountdown_module.js
@@ -1,21 +1,21 @@
 function customcountdown_module(timeSelector, titleSelector, alwaysActive, countdownFunction) {
-    if (typeof alwaysActive == 'undefined'){
+    if (typeof alwaysActive == 'undefined') {
         alwaysActive = false;
-    };    
-    
+    };
+
     socket.on('countdown', function(data) {
         if (countdownFunction !== undefined) {
             countdownFunction(time);
         }
         casparcountdownActive = !data.customActive;
-        
+
         if (data.customActive || alwaysActive) {
             $(titleSelector).text("Director's Countdown");
-            $(timeSelector).css( "color" , "#d4d5d6" );
-            (data.duration >0) ? hours = Math.floor(data.duration / 3600): hours = Math.abs(Math.ceil(data.duration / 3600));
-            (data.duration >0) ? minutes = Math.floor((data.duration - hours * 3600) / 60): minutes = Math.abs(Math.ceil((data.duration - hours * 3600) / 60));
-            (data.duration >0) ? seconds = data.duration - (minutes * 60 + hours * 3600): seconds = Math.abs(data.duration - (minutes * 60 + hours * 3600));
-            
+            $(timeSelector).css("color", "#d4d5d6");
+            (data.duration > 0) ? hours = Math.floor(data.duration / 3600): hours = Math.abs(Math.ceil(data.duration / 3600));
+            (data.duration > 0) ? minutes = Math.floor((data.duration - hours * 3600) / 60): minutes = Math.abs(Math.ceil((data.duration - hours * 3600) / 60));
+            (data.duration > 0) ? seconds = data.duration - (minutes * 60 + hours * 3600): seconds = Math.abs(data.duration - (minutes * 60 + hours * 3600));
+
             var secdot = seconds;
             var secdotonly;
             secdot = secdot.toFixed(1);
@@ -26,30 +26,30 @@ function customcountdown_module(timeSelector, titleSelector, alwaysActive, count
 
             hours = ((hours < 10 && hours >= 0) ? "0" : "") + hours;
             minutes = ((minutes < 10 && minutes >= 0) ? "0" : "") + minutes;
-            
-            if (data.duration < 0) {hours = "-" + hours}
+
+            if (data.duration < 0) { hours = "-" + hours }
             seconds = ((seconds < 10 && seconds >= 0) ? "0" : "") + seconds;
 
-           
+
 
             $(timeSelector).text(hours + ':' + minutes + ':' + seconds);
 
-if (data.duration <=15 && data.duration > 0 && data.customActive == true){
- 
-        $(timeSelector).css( "color" , "rgb(255, 20, 20)" );
-        // console.log(secdotonly);
-        
-// console.log(secdotonly);
-  } else {
-    $(timeSelector).css( "color" , "rgb(50, 150, 255)" );  
-  }
+            if (data.duration <= 15 && data.duration > 0) {
+
+                $(timeSelector).css("color", "rgb(255, 20, 20)");
+                // console.log(secdotonly);
+
+                // console.log(secdotonly);
+            } else {
+                $(timeSelector).css("color", "rgb(50, 150, 255)");
+            }
             if (data.duration < 0) {
                 // active = false;
                 socket.emit('reset custom countdown');
-               //$(timeSelector).css( "color" , "#646566" ); 
+                //$(timeSelector).css( "color" , "#646566" ); 
             }
-          }
-        
+        }
+
 
     });
 }
