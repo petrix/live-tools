7227d4ef77bdbd6f9d49c6eb3fc570943cdfde7b
petrix <yuri.petrik@gmail.com>
2018-06-19 06:15:48 +0300

    Update txtime_module.js

--- public/js/txtime_module.js
+++ public/js/txtime_module.js
@@ -1,18 +1,19 @@
 function txtime_module(timeSelector, titleSelector, divSelector, liveFunction) {
 
-    socket.on('live status', function(duration, status) {
+//     socket.on('live status', function(duration, status) {
+         socket.on('live status', function(status) {
 
-        hours = Math.floor(duration / 3600);
-        minutes = Math.floor((duration - hours * 3600) / 60);
-        seconds = duration - (minutes * 60 + hours * 3600);
+//         hours = Math.floor(duration / 3600);
+//         minutes = Math.floor((duration - hours * 3600) / 60);
+//         seconds = duration - (minutes * 60 + hours * 3600);
 
-        seconds = seconds.toFixed(0);
+//         seconds = seconds.toFixed(0);
 
-        hours = (hours < 10 ? "0" : "") + hours;
-        minutes = (minutes < 10 ? "0" : "") + minutes;
-        seconds = (seconds < 10 ? "0" : "") + seconds;
+//         hours = (hours < 10 ? "0" : "") + hours;
+//         minutes = (minutes < 10 ? "0" : "") + minutes;
+//         seconds = (seconds < 10 ? "0" : "") + seconds;
 
-        $(timeSelector).text(hours + ':' + minutes + ':' + seconds);
+//         $(timeSelector).text(hours + ':' + minutes + ':' + seconds);
 
         switch(status) {
             case 'on air':
