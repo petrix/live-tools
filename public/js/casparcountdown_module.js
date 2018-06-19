adf9a2d981f44e1131396fd09939d1391fc32fa4
petrix <yuri.petrik@gmail.com>
2018-06-19 20:29:12 +0300

    xxx

--- public/js/casparcountdown_module.js
+++ public/js/casparcountdown_module.js
@@ -5,69 +5,43 @@ function casparcountdown_module(timeSelector, pathSelector, alwaysActive, countd
     if (typeof alwaysActive == 'undefined') {
         alwaysActive = false;
     };
-
     socket.on('cg countdown path', function(path) {
-
         if (casparcountdownActive || alwaysActive) {
             if (path.length > 35) {
                 path = path.substr(0, 33) + "...";
             }
-            // console.log(path.length);
             $(pathSelector).text(path);
-
-
         }
     });
-
     socket.on('cg countdown timeData', function(time, totalTime) {
-
-        // procent time
         var procentTime = ((time * 100) / totalTime);
-
-        $('.progress-bar-success').css('width', procentTime.toFixed(2) + '%');
-        // console.log("procentTime - " + procentTime.toFixed(0) + '%');
-// console.log("time - " + time);
-// console.log("totalTime - " + totalTime);
+        $('.progress-bar-success').css('width', procentTime.toFixed(0) + '%');
         if (countdownFunction !== undefined) {
             countdownFunction(time);
         }
         if (casparcountdownActive || alwaysActive) {
-
-
             var hours = Math.floor(time / 3600);
             var minutes = Math.floor((time - hours * 3600) / 60);
             var seconds = time - (minutes * 60 + hours * 3600);
-
-
-            //var hours = Math.floor(time / 3600);
-            //var minutes = Math.floor(time / 60);
-            //var seconds = time - (minutes * 60);
             seconds = Math.floor(seconds);
-            // seconds = seconds.toFixed(0);
             hours = (hours < 10 ? "0" : "") + hours;
             minutes = (minutes < 10 ? "0" : "") + minutes;
             seconds = (seconds < 10 ? "0" : "") + seconds;
-
             $(timeSelector).text(hours + ':' + minutes + ':' + seconds);
         }
         var secdot = time;
         var secdotonly;
         secdot = secdot.toFixed(1);
-        secdotonly = (seconds - secdot) + 0.5;
         if (secdot <= 15) {
-            // console.log(secdotonly);
-            if (secdotonly >= 0) {
+            if (secdot >= 0) {
                 $(timeSelector).css("color", "rgb(255, 20, 20)");
-
             } else {
-
                 $(timeSelector).css("color", "#0000");
             }
         } else {
             $(timeSelector).css("color", "rgb(20, 255, 20)");
         }
     });
-
     socket.on('custom active', function(customActive) {
         casparcountdownActive = !customActive;
         if (!customActive) {
